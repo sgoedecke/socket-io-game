fa10c9a8ce762107a94a9d7516d835d82cc0d7fe
Sean Goedecke <sgoedecke@zendesk.com>
2017-09-02 10:42:04 +1000

    run the game client-side as well

--- index.js
+++ index.js
@@ -3,67 +3,62 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var gameInterval
-// ----------------------------------------
-// Main server code
-// ----------------------------------------
+var gameInterval, updateInterval
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+// TODO: extract below
 
 const players = {}
 
 const gameSize = 50; // 50-tile grid of possible locations
 
 function isValidSquare(newSquare) {
-	// bounds check
-	if (newSquare.x < 0 || newSquare.x > gameSize) {
-		return false
-	}
-	if (newSquare.y < 0 || newSquare.y > gameSize) {
-		return false
-	}
-	// collision check
-	var hasCollided = false
-	// console.log("new square", newSquare)
-	Object.keys(players).forEach((key) => {
-		player = players[key]	
-	// })
-	// .forEach((player) => {
-		player.squares.forEach((square) => {
-			if (square.x == newSquare.x && square.y == newSquare.y) {
-				hasCollided = true
-			}
-		})
-	})
-	if (hasCollided) { return false }
-	return true
+  // bounds check
+  if (newSquare.x < 0 || newSquare.x > gameSize) {
+    return false
+  }
+  if (newSquare.y < 0 || newSquare.y > gameSize) {
+    return false
+  }
+  // collision check
+  var hasCollided = false
+  // console.log("new square", newSquare)
+  Object.keys(players).forEach((key) => {
+    player = players[key] 
+  // })
+  // .forEach((player) => {
+    player.squares.forEach((square) => {
+      if (square.x == newSquare.x && square.y == newSquare.y) {
+        hasCollided = true
+      }
+    })
+  })
+  if (hasCollided) { return false }
+  return true
 }
 
 function movePlayer(id) {
 
-	var player = players[id]
-	var lastSquare = player.squares[player.squares.length - 1]
+  var player = players[id]
+  var lastSquare = player.squares[player.squares.length - 1]
 
-	var newSquare = {
-		x: lastSquare.x + player.accel.x,
-		y: lastSquare.y + player.accel.y
-	}
-	if (isValidSquare(newSquare)) {
-		// move the player and increment score
-		player.squares.push(newSquare)
-		if (player.squares.length % 30 == 0) { player.score++ }
-	} else {
-		// reset the player
-		player.squares = [player.squares[player.squares.length - 1]]
-		if (player.score >= 5) { player.score -= 5 }
-	}
+  var newSquare = {
+    x: lastSquare.x + player.accel.x,
+    y: lastSquare.y + player.accel.y
+  }
+  if (isValidSquare(newSquare)) {
+    // move the player and increment score
+    player.squares.push(newSquare)
+    if (player.squares.length % 30 == 0) { player.score++ }
+  } else {
+    // reset the player
+    player.squares = [player.squares[player.squares.length - 1]]
+    if (player.score >= 5) { player.score -= 5 }
+  }
 }
 
 function accelPlayer(id, x, y) {
-	players[id].accel.x = x
-	players[id].accel.y = y
+  players[id].accel.x = x
+  players[id].accel.y = y
 }
 
 // thanks SO
@@ -81,12 +76,22 @@ function stringToColour(str) {
 }
 
 function gameLoop() {
-	// move everyone around
+  // move everyone around
   Object.keys(players).forEach((playerId) => {
     let player = players[playerId]
-  	movePlayer(playerId)
+    movePlayer(playerId)
   })
+}
 
+// ----------------------------------------
+// Main server code
+// ----------------------------------------
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+function emitUpdates() {
   // tell everyone what's up
   io.emit('gameStateUpdate', players);
 }
@@ -95,7 +100,8 @@ io.on('connection', function(socket){
   console.log('User connected: ', socket.id)
   // start game if this is the first player
   if (Object.keys(players).length == 0) {
-  	gameInterval = setInterval(gameLoop, 80)
+  	gameInterval = setInterval(gameLoop, 30)
+    updateInterval = setInterval(emitUpdates, 500)
 	}
   // add player to players obj
   players[socket.id] = {
@@ -119,6 +125,7 @@ io.on('connection', function(socket){
     	io.emit('gameStateUpdate', players);
   	} else {
   		clearInterval(gameInterval)
+      clearInterval(updateInterval)
   	}
   })
 
